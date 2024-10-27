c94b5c5e26afff794ec7c5d30870278156587522
Naim <jimkhannaim97@gmail.com>
2024-10-27 23:18:44 +0600

    modified

---
+++ lib/CreatenewLabel.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: CreateActivity(),
+    );
+  }
+}
+
+class CreateActivity extends StatelessWidget{
+  const CreateActivity({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:sparknotes/Archive.dart';
+import 'package:sparknotes/CreatenewLabel.dart';
+import 'package:sparknotes/Help%20&%20feedback.dart';
+import 'package:sparknotes/Reminder.dart';
+import 'package:sparknotes/Settings.dart';
+import 'package:sparknotes/Trash.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,6 +31,16 @@ class HomeActivity extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Spark Notes'),
+        actions: [
+          IconButton(onPressed: (){}, icon: Icon(Icons.list),padding: EdgeInsets.all(3),),
+          IconButton(onPressed: (){}, icon: Icon(Icons.person),padding: EdgeInsets.all(3),),
+        ],
+        backgroundColor: Colors.white70,
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20), side: BorderSide.none),
+      ),
       drawer: Drawer(
         backgroundColor: Colors.white,
         child: ListView(
@@ -43,48 +59,44 @@ class HomeActivity extends StatelessWidget {
             ListTile(
               leading: Icon(Icons.notes),
               title: Text('Notes'),
-              onTap: () {},
+              onTap: () {Navigator.push(context,MaterialPageRoute(builder: (context)=> const HomeActivity()),);},
             ),
             ListTile(
               leading: Icon(Icons.doorbell_outlined),
               title: Text('Reminder'),
-              onTap: () {},
+              onTap: () {Navigator.push(context,MaterialPageRoute(builder: (context)=>const ReminderActivity()),);},
             ),
             ListTile(
               leading: Icon(Icons.add),
               title: Text('Create new label'),
-              onTap: () {},
+              onTap: () {Navigator.push(context,MaterialPageRoute(builder: (context)=> const CreateActivity()),);},
             ),
             ListTile(
               leading: Icon(Icons.archive_outlined),
               title: Text('Archive'),
-              onTap: () {},
+              onTap: () {Navigator.push(context,MaterialPageRoute(builder: (context)=> const ArchiveActivity()),);},
             ),
             ListTile(
               leading: Icon(Icons.restore_from_trash),
               title: Text('Trash'),
-              onTap: () {},
+              onTap: () {Navigator.push(context,MaterialPageRoute(builder: (context)=> const TrashActivity()),);},
             ),
             ListTile(
               leading: Icon(Icons.settings),
               title: Text('Settings'),
-              onTap: () {},
+              onTap: () {Navigator.push(context,MaterialPageRoute(builder: (context)=> const SettingsActivity()),);},
             ),
             ListTile(
               leading: Icon(Icons.help_outline),
               title: Text('Help & feedback'),
-              onTap: () {},
+              onTap: () {Navigator.push(context,MaterialPageRoute(builder: (context)=> const HelpActivity()),);},
             ),
           ],
         ),
       ),
       backgroundColor: Colors.black,
-      appBar: AppBar(
-        title: Text('Spark Notes'),
-        backgroundColor: Colors.white70,
-        shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20), side: BorderSide.none),
-      ),
+      bottomNavigationBar: BottomNavigationBar(items: ),
+      
     );
   }
 }
